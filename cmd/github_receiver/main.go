05618248178372a57e001ab51da14d4a74b8de99
Stephen Soltesz <stephen.soltesz@gmail.com>
2018-06-05 21:45:26 -0400

    Use new apis and update flag usage

--- cmd/github_receiver/main.go
+++ cmd/github_receiver/main.go
@@ -25,12 +25,13 @@ import (
 
 	"github.com/m-lab/alertmanager-github-receiver/alerts"
 	"github.com/m-lab/alertmanager-github-receiver/issues"
+	// TODO: add prometheus metrics for errors and github api access.
 )
 
 var (
 	authtoken       = flag.String("authtoken", "", "Oauth2 token for access to github API.")
-	githubOwner     = flag.String("owner", "", "The github user or organization name.")
-	githubRepo      = flag.String("repo", "", "The repository where issues are created.")
+	githubOwner     = flag.String("owner", "", "The github user or organization name where all repos are found.")
+	githubRepo      = flag.String("repo", "", "The default repository for creating issues when alerts do not specify an alternate repo.")
 	enableAutoClose = flag.Bool("enable-auto-close", false, "Once an alert stops firing, automatically close open issues.")
 )
 
@@ -52,8 +53,14 @@ func init() {
 }
 
 func serveListener(client *issues.Client) {
-	http.Handle("/", &issues.ListHandler{client})
-	http.Handle("/v1/receiver", &alerts.ReceiverHandler{client, *enableAutoClose})
+	http.Handle("/", &issues.ListHandler{
+		Client: client,
+	})
+	http.Handle("/v1/receiver", &alerts.ReceiverHandler{
+		Client:      client,
+		DefaultRepo: *githubRepo,
+		AutoClose:   *enableAutoClose,
+	})
 	http.ListenAndServe(":9393", nil)
 }
 
@@ -63,6 +70,6 @@ func main() {
 		flag.Usage()
 		os.Exit(1)
 	}
-	client := issues.NewClient(*githubOwner, *githubRepo, *authtoken)
+	client := issues.NewClient(*githubOwner, *authtoken)
 	serveListener(client)
 }
