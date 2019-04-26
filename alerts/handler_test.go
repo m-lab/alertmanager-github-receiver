883f4fbc8176c4c9dad4101d36b388c25a0854fa
Stephen Soltesz <stephen.soltesz@gmail.com>
2019-04-25 21:41:10 -0400

    Update alerts handler tests

--- alerts/handler_test.go
+++ alerts/handler_test.go
@@ -12,12 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //////////////////////////////////////////////////////////////////////////////
-package alerts_test
+package alerts
 
 import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -25,8 +26,7 @@ import (
 	"time"
 
 	"github.com/google/go-github/github"
-
-	"github.com/m-lab/alertmanager-github-receiver/alerts"
+	// "github.com/m-lab/alertmanager-github-receiver/alerts"
 	"github.com/prometheus/alertmanager/notify"
 	"github.com/prometheus/alertmanager/template"
 )
@@ -35,26 +35,38 @@ type fakeClient struct {
 	listIssues   []*github.Issue
 	createdIssue *github.Issue
 	closedIssue  *github.Issue
+	listError    error
+	createError  error
+	closeError   error
 }
 
 func (f *fakeClient) ListOpenIssues() ([]*github.Issue, error) {
 	fmt.Println("list open issues")
+	if f.listError != nil {
+		return nil, f.listError
+	}
 	return f.listIssues, nil
 }
 
 func (f *fakeClient) CreateIssue(repo, title, body string, extra []string) (*github.Issue, error) {
 	fmt.Println("create issue")
-	f.createdIssue = createIssue(title, body)
+	if f.createError != nil {
+		return nil, f.createError
+	}
+	f.createdIssue = createIssue(title, body, repo)
 	return f.createdIssue, nil
 }
 
 func (f *fakeClient) CloseIssue(issue *github.Issue) (*github.Issue, error) {
 	fmt.Println("close issue")
+	if f.closeError != nil {
+		return nil, f.closeError
+	}
 	f.closedIssue = issue
 	return issue, nil
 }
 
-func createWebhookMessage(alertname, status string) *bytes.Buffer {
+func createWebhookMessage(alertname, status, repo string) *notify.WebhookMessage {
 	msg := &notify.WebhookMessage{
 		Data: &template.Data{
 			Receiver: "webhook",
@@ -69,7 +81,7 @@ func createWebhookMessage(alertname, status string) *bytes.Buffer {
 				},
 			},
 			GroupLabels:  template.KV{"alertname": alertname},
-			CommonLabels: template.KV{"alertname": alertname},
+			CommonLabels: template.KV{"alertname": alertname, "repo": repo},
 			ExternalURL:  "http://localhost:9093",
 		},
 		Version:  "4",
@@ -78,94 +90,178 @@ func createWebhookMessage(alertname, status string) *bytes.Buffer {
 	if status == "resolved" {
 		msg.Data.Alerts[0].EndsAt = time.Unix(1498618000, 0)
 	}
+	return msg
+}
+
+func marshalWebhookMessage(msg *notify.WebhookMessage) *bytes.Buffer {
 	b, _ := json.Marshal(msg)
 	return bytes.NewBuffer(b)
-	// return msg
 }
 
-func createIssue(title, body string) *github.Issue {
+func createIssue(title, body, repo string) *github.Issue {
 	return &github.Issue{
-		Title: github.String(title),
-		Body:  github.String(body),
+		Title:         github.String(title),
+		Body:          github.String(body),
+		RepositoryURL: github.String(repo),
 	}
 }
 
-func TestReceiverHandler(t *testing.T) {
-	// Test: resolve an existing issue.
-	// * msg is "resolved"
-	// * issue returned by list
-	// * issue is closed
-	postBody := createWebhookMessage("DiskRunningFull", "resolved")
-	// Create a response recorder.
-	rw := httptest.NewRecorder()
-	// Create a synthetic request object for ServeHTTP.
-	req, err := http.NewRequest("POST", "/v1/receiver", postBody)
-	if err != nil {
-		t.Fatal(err)
-	}
+type errorReader struct {
+}
 
-	// Provide a pre-existing issue to close.
-	f := &fakeClient{
-		listIssues: []*github.Issue{
-			createIssue("DiskRunningFull", "body1"),
+func (e *errorReader) Read(p []byte) (n int, err error) {
+	return 0, fmt.Errorf("Fake error")
+}
+
+func TestReceiverHandler_ServeHTTP(t *testing.T) {
+	tests := []struct {
+		name           string
+		method         string
+		msgAlert       string
+		msgAlertStatus string
+		msgRepo        string
+		fakeClient     *fakeClient
+		AutoClose      bool
+		DefaultRepo    string
+		ExtraLabels    []string
+		httpStatus     int
+		wantMessageErr bool
+		wantReadErr    bool
+	}{
+		{
+			name:           "successful-close",
+			method:         http.MethodPost,
+			msgAlert:       "DiskRunningFull",
+			msgAlertStatus: "resolved",
+			fakeClient: &fakeClient{
+				listIssues: []*github.Issue{
+					createIssue("DiskRunningFull", "body1", ""),
+				},
+			},
+			AutoClose:   true,
+			DefaultRepo: "default",
+			httpStatus:  http.StatusOK,
+		},
+		{
+			name:           "successful-create",
+			method:         http.MethodPost,
+			msgAlert:       "DiskRunningFull",
+			msgAlertStatus: "firing",
+			fakeClient:     &fakeClient{},
+			AutoClose:      true,
+			DefaultRepo:    "default",
+			httpStatus:     http.StatusOK,
+		},
+		{
+			name:           "successful-create-with-explicit-repo",
+			method:         http.MethodPost,
+			msgAlert:       "DiskRunningFull",
+			msgAlertStatus: "firing",
+			msgRepo:        "custom-repo",
+			fakeClient:     &fakeClient{},
+			AutoClose:      true,
+			DefaultRepo:    "default",
+			httpStatus:     http.StatusOK,
+		},
+		{
+			name:           "successful-ignore-existing-issue-for-firing-alert",
+			method:         http.MethodPost,
+			msgAlert:       "DiskRunningFull",
+			msgAlertStatus: "firing",
+			fakeClient: &fakeClient{
+				listIssues: []*github.Issue{
+					createIssue("DiskRunningFull", "body1", ""),
+				},
+			},
+			AutoClose:   true,
+			DefaultRepo: "default",
+			httpStatus:  http.StatusOK,
+		},
+		{
+			name:           "failure-unmarshal-error",
+			method:         http.MethodPost,
+			httpStatus:     http.StatusBadRequest,
+			wantMessageErr: true,
+		},
+		{
+			name:        "failure-reader-error",
+			method:      http.MethodPost,
+			httpStatus:  http.StatusInternalServerError,
+			wantReadErr: true,
+		},
+		{
+			name:   "failure-list-error",
+			method: http.MethodPost,
+			fakeClient: &fakeClient{
+				listError: fmt.Errorf("Fake error listing current issues"),
+			},
+			AutoClose:   true,
+			DefaultRepo: "default",
+			httpStatus:  http.StatusInternalServerError,
+		},
+		{
+			name:       "failure-wrong-method",
+			method:     http.MethodGet,
+			httpStatus: http.StatusMethodNotAllowed,
 		},
 	}
-	handler := alerts.ReceiverHandler{
-		Client:      f,
-		AutoClose:   true,
-		DefaultRepo: "default",
-	}
-	handler.ServeHTTP(rw, req)
-	resp := rw.Result()
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var msgReader io.Reader
+			var msg io.ReadWriter
+			msg = marshalWebhookMessage(createWebhookMessage(tt.msgAlert, tt.msgAlertStatus, tt.msgRepo))
+			if tt.wantMessageErr {
+				// Deliberately corrupt the json content by adding extra braces.
+				msg.Write([]byte{'}', '{'})
+			}
+			msgReader = msg
+			if tt.wantReadErr {
+				msgReader = &errorReader{}
+			}
 
-	// Check the results.
-	body, _ := ioutil.ReadAll(resp.Body)
-	if resp.StatusCode != http.StatusOK {
-		t.Errorf("ReceiverHandler got %d; want %d", resp.StatusCode, http.StatusOK)
-	}
-	if f.closedIssue == nil {
-		t.Fatalf("ReceiverHandler failed to close issue")
-	}
-	if *f.closedIssue.Title != "DiskRunningFull" {
-		t.Errorf("ReceiverHandler closed wrong issue; got %q want \"DiskRunningFull\"",
-			*f.closedIssue.Title)
-	}
-	t.Logf("body: %s", body)
+			// Create a response recorder.
+			rw := httptest.NewRecorder()
+			// Create a synthetic request that sends an alertmanager webhook message.
+			req, err := http.NewRequest(tt.method, "/v1/receiver", msgReader)
+			if err != nil {
+				t.Fatal(err)
+				return
+			}
 
-	// Test: create a new issue.
-	// * msg is "firing"
-	// * issue list is empty.
-	// * issue is created
-	postBody = createWebhookMessage("DiskRunningFull", "firing")
-	// Create a response recorder.
-	rw = httptest.NewRecorder()
-	// Create a synthetic request object for ServeHTTP.
-	req, err = http.NewRequest("POST", "/v1/receiver", postBody)
-	if err != nil {
-		t.Fatal(err)
-	}
+			rh := &ReceiverHandler{
+				Client:      tt.fakeClient,
+				AutoClose:   tt.AutoClose,
+				DefaultRepo: tt.DefaultRepo,
+				ExtraLabels: tt.ExtraLabels,
+			}
+			rh.ServeHTTP(rw, req)
+			resp := rw.Result()
 
-	// No pre-existing issues to close.
-	f = &fakeClient{}
-	handler = alerts.ReceiverHandler{
-		Client:      f,
-		AutoClose:   true,
-		DefaultRepo: "default",
-	}
-	handler.ServeHTTP(rw, req)
-	resp = rw.Result()
+			// Check the results.
+			body, _ := ioutil.ReadAll(resp.Body)
+			if resp.StatusCode != tt.httpStatus {
+				t.Errorf("ReceiverHandler got %d; want %d", resp.StatusCode, tt.httpStatus)
+			}
+			if tt.fakeClient != nil && tt.fakeClient.closedIssue != nil {
+				if *tt.fakeClient.closedIssue.Title != tt.msgAlert {
+					t.Errorf("ReceiverHandler closed wrong issue; got %q want %q",
+						*tt.fakeClient.closedIssue.Title, tt.msgAlert)
+				}
+			}
+			if tt.fakeClient != nil && tt.fakeClient.createdIssue != nil {
+				if *tt.fakeClient.createdIssue.Title != tt.msgAlert {
+					t.Errorf("ReceiverHandler created wrong issue; got %q want %q",
+						*tt.fakeClient.createdIssue.Title, tt.msgAlert)
+				}
+				if tt.msgRepo != "" && *tt.fakeClient.createdIssue.RepositoryURL != tt.msgRepo {
+					t.Errorf("ReceiverHandler created wrong repo; got %q want %q",
+						*tt.fakeClient.createdIssue.RepositoryURL, tt.msgRepo)
+				}
+			}
+			if string(body) != "" {
+				t.Errorf("ReceiverHandler got %q; want empty body", string(body))
+			}
 
-	// Check the results.
-	body, _ = ioutil.ReadAll(resp.Body)
-	if resp.StatusCode != http.StatusOK {
-		t.Errorf("ReceiverHandler got %d; want %d", resp.StatusCode, http.StatusOK)
+		})
 	}
-	if f.createdIssue == nil {
-		t.Fatalf("ReceiverHandler failed to close issue")
-	}
-	if *f.createdIssue.Title != "DiskRunningFull" {
-		t.Errorf("ReceiverHandler closed wrong issue; got %q want \"DiskRunningFull\"",
-			*f.closedIssue.Title)
-	}
-	t.Logf("body: %s", body)
 }
