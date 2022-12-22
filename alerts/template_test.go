25b5839b0d2949e0c223f3569ae98fb0796740fe
Alex Orfanos <aorfanos@users.noreply.github.com>
2022-12-22 20:25:02 +0200

    fix(template/tests): fix formatIssueBody initial

--- alerts/template_test.go
+++ alerts/template_test.go
@@ -24,19 +24,61 @@ import (
 	amtmpl "github.com/prometheus/alertmanager/template"
 )
 
-// func Test_formatIssueBody(t *testing.T) {
-// 	wh := createWebhookMessage("FakeAlertName", "firing", "")
-// 	brokenTemplate := `
-// {{range .NOT_REAL_FIELD}}
-//     * {{.Status}}
-// {{end}}
-// 	`
-// 	alertTemplate = template.Must(template.New("alert").Parse(brokenTemplate))
-// 	got := formatIssueBody(wh)
-// 	if got != "" {
-// 		t.Errorf("formatIssueBody() = %q, want empty string", got)
-// 	}
-// }
+func TestFormatIssueBodySimple(t *testing.T) {
+	msg := webhook.Message{
+		Data: &amtmpl.Data{
+			Status: "firing",
+			Alerts: []amtmpl.Alert{
+				{
+					Annotations: amtmpl.KV{"env": "prod", "svc": "foo"},
+				},
+				{
+					Annotations: amtmpl.KV{"env": "stage", "svc": "foo"},
+				},
+			},
+		},
+	}
+	tests := []struct {
+		tmplTxt      string
+		expectErrTxt string
+		expectOutput string
+	}{
+		{"foo", "", "foo"},
+		{"{{ .Data.Status }}", "", "firing"},
+		{"{{ .Status }}", "", "firing"},
+		{"{{ range .NOT_REAL_FIELD }}\n* {{.Status}}\n{{end}}", "", ""},
+		{"{{ .Foo }}", "can't evaluate field Foo", ""},
+	}
+
+	for testNum, tc := range tests {
+		testName := fmt.Sprintf("tc=%d", testNum)
+		t.Run(testName, func(t *testing.T) {
+			rh, err := NewReceiver(&fakeClient{}, "default", false, "", nil, tc.tmplTxt, tc.tmplTxt)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			body, err := rh.formatIssueBody(&msg)
+			if tc.expectErrTxt == "" && err != nil {
+				t.Error(err)
+			}
+			if tc.expectErrTxt != "" {
+				if err == nil {
+					t.Error()
+				} else if !strings.Contains(err.Error(), tc.expectErrTxt) {
+					t.Error(err.Error())
+				}
+			}
+			if tc.expectOutput == "" && body != "" {
+				t.Error(body)
+			}
+			if !strings.Contains(body, tc.expectOutput) {
+				t.Error(body)
+			}
+		})
+	}
+}
+
 
 func TestFormatTitleSimple(t *testing.T) {
 	msg := webhook.Message{
